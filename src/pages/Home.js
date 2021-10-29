9e7b9f16b1b10bd80123c0046043b8d1ad75ac38
unknown <tgantara@gmail.com>
2021-10-29 22:51:14 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
         switch (activeItem) {
             case 'All':
                 return (
-                    <Popup trigger={<Card>
+                    <Popup position="right center" trigger={<Card>
                         <Icon name='play circle' size="huge" style={iconPlay} />
                         <Image src='https://images.unsplash.com/photo-1480796927426-f609979314bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80' wrapped ui={false} />
                         <Card.Content>
@@ -46,7 +46,7 @@ function Home() {
                                 <p>
                                     <b>1</b> loremipsum
                                 </p>
-                                <Button>Tambah Ke Keranjang</Button>
+                                <Button><Icon name="shopping basket"/>Tambah Ke Keranjang</Button>
                             </Grid.Column>
                         </Grid>
                     </Popup>
