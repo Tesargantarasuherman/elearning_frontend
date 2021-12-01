4785c5c600e2bffbcbf7216309e29f5551bbe993
unknown <tgantara@gmail.com>
2021-12-02 00:25:32 +0700

    update

--- src/components/Commentar.js
+++ src/components/Commentar.js
@@ -1,41 +1,63 @@
 import React from "react";
 import { Button, Comment, Form, Header } from "semantic-ui-react";
 
-const Commentar = (props) => (
-  <Comment.Group>
-    <Header as="h3" dividing>
-      Comments
-    </Header>
+const Commentar = (props) => {
+  return (
+    <>
+      <Comment.Group>
+        <Header as="h3" dividing>
+         Komentar( {parseInt(props.totalResult)})
+        </Header>
 
-    {props.isiKomentar && props.isiKomentar.length > 0 ? (
-      props.isiKomentar.map((komentar) => {
-        return (
-          <Comment>
-            <Comment.Avatar src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
-            <Comment.Content>
-              <Comment.Author as="a">{komentar.nama}</Comment.Author>
-              <Comment.Metadata>
-                <div>Today at 5:42PM</div>
-              </Comment.Metadata>
-              <Comment.Text>{komentar.isi_komentar}</Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
-          </Comment>
-        );
-      })
-    ) : (
-      <Header as="h3" dividing>
-        Tidak Ada Komentar
-      </Header>
-    )}
-
-    <Form reply>
-      <Form.TextArea />
-      <Button content="Add Reply" labelPosition="left" icon="edit" primary />
-    </Form>
-  </Comment.Group>
-);
+        {props.isiKomentar && props.isiKomentar.length > 0 ? (
+          props.isiKomentar.map((komentar) => {
+            return (
+              <>
+                <Comment>
+                  <Comment.Avatar src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+                  <Comment.Content>
+                    <Comment.Author as="a">{komentar.nama}</Comment.Author>
+                    <Comment.Metadata>
+                      <div>Today at 5:42PM</div>
+                    </Comment.Metadata>
+                    <Comment.Text>{komentar.isi_komentar}</Comment.Text>
+                    <Comment.Actions>
+                      <Comment.Action>Reply</Comment.Action>
+                    </Comment.Actions>
+                  </Comment.Content>
+                </Comment>
+              </>
+            );
+          })
+        ) : (
+          <Header as="h3" dividing>
+            Tidak Ada Komentar
+          </Header>
+        )}
+        {props.lengthKomentar < props.totalResult ? (
+          <Button
+            content="Muat Lagi"
+            disabled={props.isLoading}
+            onClick={() => props.setPage((c) => c + 1)}
+          />
+        ) : null}
+        <Form reply onSubmit={props.submitKomentar}>
+          <Form.TextArea
+            name="isi_komentar"
+            onChange={props.handleFormKomentar}
+            value={props.formKomentar.isi_komentar}
+          />
+          <Button
+            type="submit"
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+          />
+        </Form>
+      </Comment.Group>
+    </>
+  );
+};
 
 export default Commentar;
--- src/pages/MyClass.js
+++ src/pages/MyClass.js
@@ -1,41 +1,130 @@
 import axios from "axios";
 import React from "react";
-import { useState, useEffect,useContext } from "react";
-import { Menu, Embed, Grid } from "semantic-ui-react";
+import { useState, useEffect, useContext } from "react";
+import { Menu, Embed, Grid, Button } from "semantic-ui-react";
 import Commentar from "../components/Commentar";
 import { AuthContext } from "../context/AuthContext";
 import { useParams } from "react-router-dom";
 import BaseUrl from "../utils/BaseUrl";
 import axiosConfig from "../utils/Config";
+const defaultData = {
+  status: "ok",
+  totalResult: 10,
+  articles: [],
+};
+const endpoint = "http://localhost:8000/api/v1/komentar/1/1/1";
+let rows = [];
 
 export default function MyClass() {
   const [state, setstate] = useState({ activeItem: null });
   const [data_class, setdata_class] = useState([]);
   const [materi, setmateri] = useState({});
   const { login, setLogin } = useContext(AuthContext);
-  const [isiKomentar,setIsiKomentar] = useState([])
+  const [isiKomentar, setIsiKomentar] = useState([]);
 
-  const handleItemClick = (materi, row) => {
-    setstate({ activeItem: materi.judul });
-    setmateri(materi);
-    getKomentar(materi)
+  const [stateData, setstateData] = useState(defaultData);
+  const [page, setPage] = useState(1);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [isRefresh, setRefresh] = useState(false);
+
+  const [formKomentar, setFormKomentar] = useState({
+    user_id: "",
+    materi_id: "",
+    kursus_id: "",
+    kelas_id: "",
+    isi_komentar: "",
+  });
+  const handleRefresh = () => {
+    setstateData(defaultData);
+    setPage(1);
+    setLoading(false);
+    setRefresh(false);
+  };
+
+  const handleItemClick = (_materi, row) => {
+    setstate({ activeItem: _materi.judul });
+    setmateri(_materi);
+    getKomentar(_materi);
   };
 
   let { id } = useParams();
 
   useEffect(() => {
-    axios.get(`${BaseUrl}kursus/${id}/${login.data.id}`,axiosConfig).then((res) => {
-      console.log(res)
-      setdata_class(res.data.data.data_kelas);
+    getKursus()
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${endpoint}?page=${page}`);
+        const result = await response.json();
+        setstateData((current) => {
+          return {
+            ...result,
+            articles: [...current.articles, ...result.data.data],
+            totalResult: result.data.total,
+            status: result.status,
+          };
+        });
+        if (result.status !== "ok") {
+          throw new Error("error");
+        }
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [page, isRefresh]);
+
+  const getKursus = () => {
+    axios
+      .get(`${BaseUrl}kursus/${id}/${login.data.id}`, axiosConfig)
+      .then((res) => {
+        console.log(res);
+        setdata_class(res.data.data.data_kelas);
+      });
+  };
+  const getKomentar = (_materi) => {
+    axios
+      .get(`${BaseUrl}komentar/${id}/${_materi.kelas_id}/${_materi.id}`)
+      .then((res) => {
+        setIsiKomentar(res.data.data.data);
+      });
+  };
+  const handleFormKomentar = (e) => {
+    const { name, value } = e.target;
+
+    setFormKomentar({
+      ...formKomentar,
+      [name]: value,
+      user_id: `${login.data.id}`,
+      materi_id: `${materi.id}`,
+      kursus_id: id,
+      kelas_id: `${materi.kelas_id}`,
     });
-  }, []);
-
-  const getKomentar=(materi)=>{
-    axios.get(`${BaseUrl}komentar/${id}/${materi.kelas_id}/${materi.id}`).then(res=>{
-      setIsiKomentar(res.data.data)
-    })
-  }
-
+    console.log(formKomentar);
+  };
+  const submitKomentar = () => {
+    axios
+      .post(`${BaseUrl}add-komentar`, formKomentar, axiosConfig)
+      .then((res) => {
+        setFormKomentar({
+          ...formKomentar,
+          isi_komentar: "",
+          user_id: "",
+          materi_id: "",
+          kursus_id: "",
+          kelas_id: "",
+        });
+      });
+    getKomentar(materi);
+  };
+  const paginateKomentar = (lengthMateri) => {
+    for (let i = 1; i >= lengthMateri; i++) {
+      this.rows.push(<span class="fa fa-star"></span>);
+    }
+    console.log(rows);
+  };
   return (
     <div className="container">
       <Grid divided="vertically">
@@ -72,7 +161,16 @@ export default function MyClass() {
               source="youtube"
             />
             <h3>{materi.deskripsi}</h3>
-            <Commentar isiKomentar ={isiKomentar}/>
+            <Commentar
+              isiKomentar={stateData.articles}
+              handleFormKomentar={handleFormKomentar}
+              submitKomentar={submitKomentar}
+              formKomentar={formKomentar}
+              lengthKomentar={stateData.articles.length}
+              totalResult={stateData.totalResult}
+              setPage={setPage}
+              isLoading={isLoading}
+            />
           </Grid.Column>
         </Grid.Row>
       </Grid>
