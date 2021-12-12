0f13b3de69078ea2393d3de7a64880d90b0e1251
unknown <tgantara@gmail.com>
2021-12-12 21:33:19 +0700

    update

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import '../Jumbotron/Styles/Jumbotron.css'
 import imgJumbotron from '../../images/BG3.png'
+import { useTranslation } from 'react-i18next';
+
 function Jumbotron() {
+    const { t, i18n } = useTranslation();
     return (
         <section className='jumbotron'>
             <div className='jumbotron-left'>
                 <h1>
-                    <strong>Learn Japanese With Us</strong>
+                    <strong>{t('Jumbotron.1')}</strong>
                 </h1>
                 <h2>
-                    Let's Begin Now
+                    {t('Jumbotron.2')}
                 </h2>
                 <div>
                     <button>Register</button>
