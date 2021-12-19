06026259a1f1055d7bf8a4918f70da41d671a607
unknown <tgantara@gmail.com>
2021-12-20 00:56:02 +0700

    profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import './Profile.css'
 function Profile() {
     return (
-        <div>
-            My Profile
+        <div className="my-profile">
+            <form action="" className="form-profile">
+                <div className="body-input-form-profile">
+                    <label htmlFor="">Nama Lengkap</label>
+                    <input type="text" />
+                </div>
+                <div className="body-input-form-profile">
+                    <label htmlFor="">Email</label>
+                    <input type="text" />
+                </div>
+                <div className="body-input-form-profile">
+                    <label htmlFor="">No Kontak</label>
+                    <input type="text" />
+                </div>
+                <div className="body-input-form-profile">
+                    <label htmlFor="">Alamat</label>
+                    <input type="text" />
+                </div>
+                <div className="body-input-form-profile">
+                    <label htmlFor="">No Rekening</label>
+                    <input type="text" />
+                </div>
+                <button>Update</button>
+            </form>
         </div>
     )
 }
