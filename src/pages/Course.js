7dd89a2f4188f13d4d50ca03c40c67f8e6d72364
unknown <tgantara@gmail.com>
2021-12-21 23:46:09 +0700

    update

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import ClassCourse from '../components/ClassCourse/ClassCourse'
+import BaseUrl from "../utils/BaseUrl";
 
 function Course() {
+    const [dataCourse, setDataCourse] = useState([]);
+
+    useEffect(() => {
+      axios.get(`${BaseUrl}kursus`).then((res) => {
+        setDataCourse(res.data.data);
+      });
+    }, []);
     return (
-        <ClassCourse />
+        <ClassCourse dataCourse={dataCourse}/>
     )
 }
 
